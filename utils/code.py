269db0d4aee49e1e1cfd45dc465d557d0f651cd4
Takamichi Omori <merrytakamerry@gmail.com>
2020-05-17 22:44:50 +0900

    update docker

--- utils/code.py
+++ utils/code.py
@@ -1,9 +1,2 @@
-n,k=map(int,input().split())
-a=list(map(int,input().split()))
-cnt=k
-for i in range(n):
-    cnt +=a[i]
-    if cnt<=0:
-        print('No')
-        exit()
-print('Yes'if cnt>k else 'No')
+x,y=map(int,input().split())
+print(x+y)
